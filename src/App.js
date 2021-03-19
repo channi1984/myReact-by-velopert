1b10435c330051126b3a5afd060547e7cdadc7c7
channi <channi.kr@gmail.com>
2021-03-19 11:34:15 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import EventPractice from './EventPractice';
 import MyComponent from './MyComponent'
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="box">
         <MyComponent name={11} age={2} />
+        <EventPractice />
       </div>
     )
   }
