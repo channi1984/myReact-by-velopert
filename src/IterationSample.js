2732633ebc014fb39c4b6505e1ae23248523044b
channi <channi.kr@gmail.com>
2021-03-19 15:32:52 +0900

    [commit]

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 
 class IterationSample extends Component {
+    state = {
+        names: ['눈사람', '얼음', '눈', '바람'],
+        name: ''
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    handleInsert = () => {
+        this.setState({
+            names: this.state.names.concat(this.state.name),
+            name: ''
+        })
+    }
+
     render() {
-        const names = ['눈사람', '얼음', '눈', '바람'];
-        const nameList = names.map(
+        const nameList = this.state.names.map(
             (name, index) => (<li key={index}> {name} </li>)
         );
 
         return (
-            <ul>
-                {nameList}
-            </ul>
+            <div>
+                <input
+                    onChange={this.handleChange}
+                    value={this.state.name}
+                />
+                <button onClick={this.handleInsert}>추가</button>
+                <ul>
+                    {nameList}
+                </ul>
+            </div>
         );
     }
 }
